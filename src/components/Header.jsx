71aead47fae8eccd8444b8fef5ff32ff245e0212
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-11-17 01:55:24 +0300

    feat: added  header and main components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-cyan-500 text-white text-center p-4 w-full">
+      <h1 className="text-3xl font-bold">Taurex Employee List</h1>
+    </header>
+  );
+};
+
+export default Header;
